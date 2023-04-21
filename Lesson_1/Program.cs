9fafc72caa30a1ba0f8e81ed94872824eab8a27d
«Julia» <juliya454@yandex.ru>
2023-04-21 21:50:37 +0300

    Закончили 1 лекцию

--- Lesson_1/Program.cs
+++ Lesson_1/Program.cs
@@ -1,6 +1,77 @@
-﻿Console.Write("Введите ваше имя: ");
-string username = Console.ReadLine();
-Console.Write("Привет, ");
-Console.Write(username);
+﻿// Console.Write("Введите ваше имя: ");
+// string username = Console.ReadLine();
+// Console.Write("Привет, ");
+// Console.Write(username);
 
 
+// int numberA = 33;
+// int numberB = 5;
+// int result = (numberA + numberB);
+// Console.WriteLine(result);
+
+
+// double numberA = 12;
+// double numberB = 5;
+// Console.WriteLine(numberA / numberB);
+
+
+// int numberA = new Random ().Next(1, 10);
+// Console.WriteLine(numberA);
+// int numberB = new Random ().Next(1, 10);
+// Console.WriteLine(numberB);
+// int result = (numberA + numberB);
+// Console.WriteLine(result);
+
+
+// Console.Write("Введите имя пользователя: ");
+// string username = Console.ReadLine();
+
+// if(username.ToLower() == "юля")
+// {
+//     Console.WriteLine("Ура, это же Юля!");
+// }
+
+// else
+// {
+// Console.Write("Привет, ");
+// Console.WriteLine(username);
+// }
+
+
+// int a = 1;
+// int b = 2;
+// int c = 6;
+// int d = 8;
+// int e = 4;
+
+// int max = a;
+
+// if (a>b) max = a;
+// if (b>max) max = b;
+// if (c>max) max = c;
+// if (d>max) max = d;
+// if (e>max) max = e;
+
+// Console.Write("max = ");
+// Console.WriteLine(max);
+
+
+Console.Clear();
+// Console.SetCursorPosition(10, 4);
+// Console.WriteLine("+");
+
+// int xa = 1;
+// int ya = 1;
+// int xb = 1;
+// int yb = 30;
+// int xc = 80; 
+// int yc = 30;
+
+// Console.SetCursorPosition(xa, ya);
+// Console.WriteLine("+");
+
+// Console.SetCursorPosition(xb, yb);
+// Console.WriteLine("+");
+
+// Console.SetCursorPosition(xc, yc);
+// Console.WriteLine("+");
